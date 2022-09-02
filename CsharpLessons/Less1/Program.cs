9d68c5bfda0b894681f3e20a239faaaa3154a237
Yuriy <ozornin-v@yandex.ru>
2022-09-03 02:08:37 +0500

    старт

---
+++ CsharpLessons/Less1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpLesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+        }
+    }
+}
